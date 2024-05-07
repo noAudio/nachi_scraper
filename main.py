1b61c83d459b803655ad6af866e8055b06112f89
Samuel Odongo <samieodd@gmail.com>
2024-05-07 21:37:09 +0300

    Write results to file

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import csv
 from scraper import Scraper
 
 
 def main() -> None:
     scraper: Scraper = Scraper()
     scraper.getData()
+    print(len(scraper.results), 'inspectors')
+    for inspector in scraper.results:
+        inspector.website = scraper.getWebsite(inspector.profileLink)
+        if inspector.website != 'n/a':
+            index: int = scraper.results.index(inspector) + 1
+            print(
+                f'[{index}/{len(scraper.results)}: {inspector.name}, {inspector.profileLink}, {inspector.website}]')
+    with open(f'inspectors.csv', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Name', 'Name of Company', 'Phone Number', 'Website'])
+        for inspector in scraper.results:
+            writer.writerow([f"{inspector.name.replace('"', '')}", f"{inspector.company.replace(
+                '"', '')}", inspector.phone, f"{inspector.website.replace('"', '')}"])
+        f.close()
 
 
 if __name__ == '__main__':
